8a66b56eb59037890f0156110841ce68c823a66e
Aaron Durant <aaron@aarondurant.com>
2023-01-23 15:41:45 -0500

    Initial Commit

---
+++ MyWorkouts Watch App/MyWorkoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWorkoutsApp.swift
+//  MyWorkouts Watch App
+//
+//  Created by Aaron Durant on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct MyWorkouts_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
